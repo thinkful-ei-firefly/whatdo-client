8a521f43effb2882520fb943a4e9ad884e0e4450
krill <lpmaze@gmail.com>
2019-10-21 15:35:55 -0400

    added browser router, connected Route to landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,13 @@ import {Switch, Route} from 'react-router-dom'
 
 
 import './App.css';
+import LandingPage from '../../routes/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path={'/'} component={''}/>
+        <Route exact path={'/'} component={LandingPage}/>
         {/* PublicOnlyRoute
             PrivateOnlyRoute */}
         <Route component={'NotFound'}/>
