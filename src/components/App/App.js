d75a3eb07f1af5b6b1c755122c3ecec9f74f3aa0
krill <lpmaze@gmail.com>
2019-10-21 15:30:17 -0400

    added react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path={'/'} component={''}/>
+        {/* PublicOnlyRoute
+            PrivateOnlyRoute */}
+        <Route component={'NotFound'}/>
+      </Switch>
+
     </div>
   );
 }
