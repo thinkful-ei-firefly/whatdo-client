87ba48453f8e1c09d55a635a597c6e212c555551
krill <lpmaze@gmail.com>
2019-10-22 13:42:02 -0400

    working on token service, added submit SignInForm

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import AuthApiService from '../../services/auth-api-service'
+import TokenService from '../../services/token-service'
 
 export default class SignInForm extends React.Component{
+  state = {error: null, loading: false}
 
   handleSubmit = async e => {
     e.preventDefault()
-    console.log('Clicked LogIn')
+    // console.log('Clicked LogIn')
+    const {username, password} = e.target
+    // console.log(username.value, password.value)
+    const loginCredentials = {
+      username: username.value,
+      password: password.value
+    }
+
+    try{
+      const user = await AuthApiService.login(loginCredentials)
+      TokenService.saveAuthToken(user.authToken)
+
+    } catch(err){
+
+    }
+  
   }
 
   render(){
@@ -15,7 +33,7 @@ export default class SignInForm extends React.Component{
           <input type="text" id="username" name="username" required/>
 
           <label htmlFor="pass">Password</label>
-          <input type="password" id="pass" name="pass" required />
+          <input type="password" id="password" name="password" required />
 
           <input type="submit" value="Sign In"/>
         </div>
