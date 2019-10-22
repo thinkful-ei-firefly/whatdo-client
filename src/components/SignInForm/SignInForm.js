45f1203417658d785901d97503d532a373fab7e6
krill <lpmaze@gmail.com>
2019-10-22 11:32:57 -0400

    SignIn jsx/route added

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-export default class LoginPage extends React.Component{
+export default class SignInForm extends React.Component{
+
+  handleSubmit = async e => {
+    console.log('Clicked LogIn')
+  }
 
   render(){
     return(
-      <div className='LoginPage'>
-        
-      </div>
+      <form className='SignInForm' onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required/>
+
+          <label htmlFor="pass">Password</label>
+          <input type="password" id="pass" name="pass" required />
+
+          <input type="submit" value="Sign In"/>
+        </div>
+      </form>
     )
   }
 }
--- src/routes/SignInPage/SignInPage.js
+++ src/routes/SignInPage/SignInPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import SignInForm from '../../components/SignInForm/SignInForm'
+
 export default class LoginPage extends React.Component{
 
   render(){
     return(
       <div className='LoginPage'>
-        
+        <SignInForm />
       </div>
     )
   }
