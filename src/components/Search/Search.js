89eb461125ba4d0eac93a2015619fd7ce214df14
Ahmed <ahmed.max7586@gmail.com>
2019-10-23 12:13:06 -0400

    done with base @ search page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,26 +1,60 @@
 import React from 'react'
-
+import './Search.css'
 export default class Search extends React.Component {
   render() {
     return (
       <div className="Search">
-        <form className="search-form">
-          <div>
-            <input
-              placeholder="zipcode"
-              type="text"
-              name="search"
-              id="search"
-            />
-            <input
-              placeholder="within X mile"
-              type="text"
-              name="search"
-              id="search"
-            />
-          </div>
+      <h1>
+      Search event near you
+    </h1>
+    <h3>"Have fun, be yourself ,enjoy life and stay positive" - Tatiana Maslany</h3>
 
+        <form className='form'  onSubmit={this.handleSubmit} >
+      {/**
+      <div role='alert' className='alert' >
+          {error && <p>{error}</p>}
+        </div>
+      */}
+        
+
+        <div className='form-section'>
+          
+          <input type="text"
+          className='input'
+          
+            id='zipcode'
+            name='zipcode'
+            autoComplete='off'
+            required
+          />
+          <label 
+          className='label-name'
+          htmlFor='zipcode'>
+          <span className='content-name' >Enter your zipcode</span>  
+          </label>
+        </div>
+
+        <div className='form-section'>
+          
           <input
+          type="text"
+          className='input'
+          
+            id='mile'
+            name='mile'
+            autoComplete='off'
+            required
+          />
+          <label 
+          className='label-name'
+          htmlFor='mile'>
+          <span className='content-name' >Mile around you</span>  
+          </label>
+        </div>
+
+          <div className='form-section'>
+          <input
+          className='input'
             type="date"
             id="date"
             name="date"
@@ -28,81 +62,42 @@ export default class Search extends React.Component {
             min="2019-01-01"
             max="2019-12-31"
           />
+          </div>
+          
 
-          <p>Select event type</p>
+          <h3>Select event type</h3>
 
-          <input
-            type="radio"
-            name="event-type"
-            value="1"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Concert</span>
-          </label>
+          <div className='form-section checkbox__section'>
 
-          <input
-            type="radio"
-            name="event-type"
-            value="2"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type" />
-          <span>Outdoor Activities</span>
+        <input type="checkbox" id='box1' name='box1' value='Concert'></input>
+        <label htmlFor="box1" >Concert</label>
+        <input type="checkbox" id='box2' name='box2' value='Sports'></input>
+        <label htmlFor="box2" >Sports</label>
+        <input type="checkbox" id='box3' name='box3' value='Museum'></input>
+        <label htmlFor="box3" >Museum</label>
+        <br></br>
+        <input type="checkbox" id='box4' name='box4' value='Arts'></input>
+        <label htmlFor="box4" >Arts</label>
+        <input type="checkbox" id='box5' name='box5' value='Bars'></input>
+        <label htmlFor="box5" >Bars</label>
+        <input type="checkbox" id='box6' name='box6' value='Other'></input>
+        <label htmlFor="box6" >Other</label>
 
-          <input
-            type="radio"
-            name="event-type"
-            value="1"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Museums</span>
-          </label>
 
-          <input
-            type="radio"
-            name="event-type"
-            value="2"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Festivals</span>
-          </label>
+          </div>
 
-          <input
-            type="radio"
-            name="event-type"
-            value="2"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Bars</span>
-          </label>
-
-          <input
-            type="radio"
-            name="event-type"
-            value="2"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Movies</span>
-          </label>
-
-          <input
-            type="radio"
-            name="event-type"
-            value="2"
-            className="event-type-radio"
-          />
-          <label htmlFor="event-type">
-            <span>Others</span>
-          </label>
-
-          <input type="submit" value='Search'/>
+          <div className='form-btn' >
+        <button className='searchForm__btn' type='submit'>
+          Search
+        </button>
+        </div>
+        
+        
         </form>
       </div>
     )
   }
 }
+
+
+
