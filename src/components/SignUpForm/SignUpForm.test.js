5c99c1c3cfcd0e6cac2d4737a2b59aa204b08fb6
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-24 17:32:43 -0400

    Add BrowserRouter for 12 smoke tests passing

--- src/components/SignUpForm/SignUpForm.test.js
+++ src/components/SignUpForm/SignUpForm.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<SignUpForm />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <SignUpForm />
+    </BrowserRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
