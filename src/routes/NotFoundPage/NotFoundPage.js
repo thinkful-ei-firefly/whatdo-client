d499d419d4aa1e4be35c22f08a95fd585c9db002
Ahmed <ahmed.max7586@gmail.com>
2019-10-24 03:37:34 -0400

    added animation @  notfoundpage

--- src/routes/NotFoundPage/NotFoundPage.js
+++ src/routes/NotFoundPage/NotFoundPage.js
@@ -5,7 +5,7 @@ export default function NotFoundPage(){
   return (
     <div className='NotFoundPage'>
     <div className='container'>
-    <h2>Oops! path not found</h2>
+    <h2>Oops! page not found</h2>
     <h1>404 </h1>
     <p>We can't find the  page you're looking for.</p>
     </div> 
