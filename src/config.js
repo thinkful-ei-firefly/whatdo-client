d233ca800645f048ce577eda6f784ec29c6a12ae
krill <lpmaze@gmail.com>
2019-10-28 11:08:37 -0400

    changed all http to https

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://thinkful-whatdo.herokuapp.com/api',//'http://localhost:8000/api',
   REACT_APP_TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'dev-client-auth-token',
-  EVENTFUL_SEARCH_ENDPOINT: 'http://api.eventful.com/json/events/search?app_key=xM3Wdb6GJm4W2MSh',
-  EVENTFUL_SINGLE_EVENT_ENDPOINT: 'http://api.eventful.com/json/events/get?app_key=xM3Wdb6GJm4W2MSh',
-  OPEN_WEATHER_FORECAST_ENDPOINT: 'http://api.openweathermap.org/data/2.5/forecast?APPID=b762cee79d34458a0c6aadef118e64c3'
+  EVENTFUL_SEARCH_ENDPOINT: 'https://api.eventful.com/json/events/search?app_key=xM3Wdb6GJm4W2MSh',
+  EVENTFUL_SINGLE_EVENT_ENDPOINT: 'https://api.eventful.com/json/events/get?app_key=xM3Wdb6GJm4W2MSh',
+  OPEN_WEATHER_FORECAST_ENDPOINT: 'https://api.openweathermap.org/data/2.5/forecast?APPID=b762cee79d34458a0c6aadef118e64c3'
 }
