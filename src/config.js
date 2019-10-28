5b2da3f046272091fc9e569c14e6ca2b8a65dbf9
krill <lpmaze@gmail.com>
2019-10-28 11:05:12 -0400

    changed all http to https

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   API_ENDPOINT: 'https://thinkful-whatdo.herokuapp.com/api',
   // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'dev-client-auth-token',
-  EVENTFUL_SEARCH_ENDPOINT: 'http://api.eventful.com/json/events/search?app_key=xM3Wdb6GJm4W2MSh',
-  EVENTFUL_SINGLE_EVENT_ENDPOINT: 'http://api.eventful.com/json/events/get?app_key=xM3Wdb6GJm4W2MSh',
-  OPEN_WEATHER_FORECAST_ENDPOINT: 'http://api.openweathermap.org/data/2.5/forecast?APPID=b762cee79d34458a0c6aadef118e64c3'
+  EVENTFUL_SEARCH_ENDPOINT: 'https://api.eventful.com/json/events/search?app_key=xM3Wdb6GJm4W2MSh',
+  EVENTFUL_SINGLE_EVENT_ENDPOINT: 'https://api.eventful.com/json/events/get?app_key=xM3Wdb6GJm4W2MSh',
+  OPEN_WEATHER_FORECAST_ENDPOINT: 'https://api.openweathermap.org/data/2.5/forecast?APPID=b762cee79d34458a0c6aadef118e64c3'
 }
