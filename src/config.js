83c82f451c676cc6f77a6e186494c13a051e2ec1
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-28 14:10:10 -0400

    pulling changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: 'https://thinkful-whatdo.herokuapp.com/api',
-  // API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'https://thinkful-whatdo.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'dev-client-auth-token',
   EVENTFUL_SEARCH_ENDPOINT: 'http://api.eventful.com/json/events/search?app_key=xM3Wdb6GJm4W2MSh',
   EVENTFUL_SINGLE_EVENT_ENDPOINT: 'http://api.eventful.com/json/events/get?app_key=xM3Wdb6GJm4W2MSh',
