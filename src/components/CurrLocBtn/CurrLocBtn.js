dd0d9d45a0f0f8f12a57bda3a12bdd9a6246461a
krill <lpmaze@gmail.com>
2019-10-29 15:27:33 -0400

    added CurrLocBtn

---
+++ src/components/CurrLocBtn/CurrLocBtn.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import GeocodingApiService from '../../services/geocoding-api-service'
+import {SearchProvider} from '../../contexts/SearchContext'
+
+export default class CurrLocBtn extends React.Component{
+  static contextType = SearchContext
+  state = {loading: false}
+
+  getLocation = () => {
+    this.setState({loading: true})
+
+    const options = {
+      timeout: 5000,
+      enableHighAccuracy: true,
+      maximumAge: 0
+    }
+
+    const success = async (pos) => {
+      const {latitude, longitude} = pos.coords
+      try{
+        const res = await GeocodingApiService.getZip(latitude, longitude)
+        console.log(res.results[0].address_components[0].long_name)
+        // this.context.
+      } catch(err){
+        console.log(err)
+      }
+      this.setState({loading: false})
+    }
+
+    const error = (err) => {
+      console.log(err)
+      this.setState({loading: false})
+    }
+
+    const currLoc = navigator.geolocation.getCurrentPosition(success, error, options)
+    
+  }
+
+  render(){
+    return(
+      <button onClick={this.getLocation}>Current Location</button>
+    )
+  }
+}
