be8d92e2076b8fc83454b0ddfbff80a8bc0351e5
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-29 14:57:47 -0400

    Add search terms to context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,102 +1,115 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import eventDataHelpers from '../../helpers/event-data-helpers'
-import EventApiService from '../../services/event-api-service'
-import weatherDataHelpers from '../../helpers/weather-data-helpers'
-import WeatherApiService from '../../services/weather-api-service'
+import eventDataHelpers from '../../helpers/event-data-helpers';
+import EventApiService from '../../services/event-api-service';
+import weatherDataHelpers from '../../helpers/weather-data-helpers';
+import WeatherApiService from '../../services/weather-api-service';
 
-import PublicOnlyRoute from '../../components/utils/PublicRoute'
-import PrivateOnlyRoute from '../../components/utils/PrivateRoute'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import PublicOnlyRoute from '../../components/utils/PublicRoute';
+import PrivateOnlyRoute from '../../components/utils/PrivateRoute';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
-import SearchContext from '../../contexts/SearchContext'
+import SearchContext from '../../contexts/SearchContext';
 
-import './App.css'
+import './App.css';
 
-import LandingPage from '../../routes/LandingPage/LandingPage'
-import EventPage from '../../routes/EventPage/EventPage'
-import EventsPage from '../../routes/EventsPage/EventsPage'
-import SignInPage from '../../routes/SignInPage/SignInPage'
-import SignUpPage from '../../routes/SignUpPage/SignUpPage'
-import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
-import MyEventsPage from '../../routes/MyEventsPage/MyEventsPage'
+import LandingPage from '../../routes/LandingPage/LandingPage';
+import EventPage from '../../routes/EventPage/EventPage';
+import EventsPage from '../../routes/EventsPage/EventsPage';
+import SignInPage from '../../routes/SignInPage/SignInPage';
+import SignUpPage from '../../routes/SignUpPage/SignUpPage';
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
+import MyEventsPage from '../../routes/MyEventsPage/MyEventsPage';
 
 class App extends React.Component {
-
   state = {
     city: 'Tucker',
     zipCode: '30084',
     weather: [],
     date: new Date(Date.now()),
     events: [],
-    savedEvents: []
-  }
+    savedEvents: [],
+    distance: '10',
+    categories: ''
+  };
 
   apiSearch = async (
-    zipCode, 
-    distance=10, 
-    date, 
-    categories=eventDataHelpers.allCatStr(), 
-    pageNum=1, 
-    pageSize=10) => {
-      //send api request to get weather
-      //send api request to get events
-      //update state
-      try {
-        const eventJsonData = await EventApiService.eventSearch(zipCode, distance, eventDataHelpers.searchDateStr(date), categories, pageNum, pageSize)
-        const eventList = eventDataHelpers.parseEventList(eventJsonData)
+    zipCode,
+    distance = 10,
+    date,
+    categories = eventDataHelpers.allCatStr(),
+    pageNum = 1,
+    pageSize = 10
+  ) => {
+    //send api request to get weather
+    //send api request to get events
+    //update state
+    try {
+      const eventJsonData = await EventApiService.eventSearch(
+        zipCode,
+        distance,
+        eventDataHelpers.searchDateStr(date),
+        categories,
+        pageNum,
+        pageSize
+      );
+      const eventList = eventDataHelpers.parseEventList(eventJsonData);
 
-        const weatherJsonData = await WeatherApiService.weatherReport(zipCode)
-        const weatherReport = weatherDataHelpers.parseWeatherReport(weatherJsonData)
+      const weatherJsonData = await WeatherApiService.weatherReport(zipCode);
+      const weatherReport = weatherDataHelpers.parseWeatherReport(
+        weatherJsonData
+      );
 
-        const newState = {
-          city: weatherJsonData.city.name,
-          zipCode,
-          weather: weatherReport,
-          date: date,
-          events: eventList
-        }
+      const newState = {
+        city: weatherJsonData.city.name,
+        zipCode,
+        weather: weatherReport,
+        date: date,
+        events: eventList,
+        distance: distance,
+        categories: categories
+      };
 
-        this.setState(newState)
-  
-      } catch(err) {
-        console.log(err)
-      }
-  }
+      this.setState(newState);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   getSavedEvents = async () => {
     try {
-      const myEvents = await EventApiService.getEventsFromDB()
-      this.setState({savedEvents: myEvents.events})
+      const myEvents = await EventApiService.getEventsFromDB();
+      this.setState({ savedEvents: myEvents.events });
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
 
-  saveEvent = async (event) => {
+  saveEvent = async event => {
     try {
-      const newFavorite = await EventApiService.postEventToDB(event)
-      this.setState({savedEvents: [...this.state.savedEvents, newFavorite]})
+      const newFavorite = await EventApiService.postEventToDB(event);
+      this.setState({ savedEvents: [...this.state.savedEvents, newFavorite] });
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }    
-  }
+  };
 
-  removeEvent = async (eventId) => {
+  removeEvent = async eventId => {
     try {
-      EventApiService.deleteEventFromDB(eventId)
-      this.setState({savedEvents: this.state.savedEvents.filter(event => event.id !== eventId)})
+      EventApiService.deleteEventFromDB(eventId);
+      this.setState({
+        savedEvents: this.state.savedEvents.filter(
+          event => event.id !== eventId
+        )
+      });
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
 
-  render () {
+  render() {
     const searchContextValue = {
       city: this.state.city,
       zipCode: this.state.zipCode,
@@ -104,12 +117,14 @@ class App extends React.Component {
       date: this.state.date,
       events: this.state.events,
       savedEvents: this.state.savedEvents,
+      distance: this.state.distance,
+      categories: this.state.categories,
       apiSearch: this.apiSearch,
       getSavedEvents: this.getSavedEvents,
       saveEvent: this.saveEvent,
       removeEvent: this.removeEvent
-    }
-    
+    };
+
     return (
       <div className="App">
         <SearchContext.Provider value={searchContextValue}>
@@ -130,8 +145,8 @@ class App extends React.Component {
           <Footer />
         </SearchContext.Provider>
       </div>
-    )
-  }  
+    );
+  }
 }
 
-export default App
+export default App;
