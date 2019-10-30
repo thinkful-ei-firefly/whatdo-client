638bce0815f793a9e2ec6b34443f744e1161f6e7
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-30 13:47:09 -0400

    load curr loc into context

--- src/components/CurrLocBtn/CurrLocBtn.js
+++ src/components/CurrLocBtn/CurrLocBtn.js
@@ -1,48 +1,50 @@
-import React from 'react'
-import GeocodingApiService from '../../services/geocoding-api-service'
-// import {SearchProvider} from '../../contexts/SearchContext'
+import React from 'react';
+import GeocodingApiService from '../../services/geocoding-api-service';
+import SearchContext from '../../contexts/SearchContext';
 
-import './CurrLocBtn.css'
+import './CurrLocBtn.css';
 
-export default class CurrLocBtn extends React.Component{
-  // static contextType = SearchContext
-  state = {loading: false}
+export default class CurrLocBtn extends React.Component {
+  static contextType = SearchContext;
+  state = { loading: false };
 
   getLocation = () => {
-    this.setState({loading: true})
+    this.setState({ loading: true });
 
     const options = {
       timeout: 5000,
       enableHighAccuracy: false,
       maximumAge: 0
-    }
+    };
 
-    const success = async (pos) => {
-      const {latitude, longitude} = pos.coords
-      try{
-        const res = await GeocodingApiService.getZip(latitude, longitude)
-        console.log(res.results[0].address_components[0].long_name)
-        // this.context.
-      } catch(err){
-        console.log(err)
+    const success = async pos => {
+      try {
+        const { latitude, longitude } = pos.coords;
+        const res = await GeocodingApiService.getZip(latitude, longitude);
+        const zipCode = res.results[0].address_components[0].long_name;
+        this.context.setZipCode(zipCode);
+        this.setState({ loading: false });
+      } catch (err) {
+        console.log(err);
       }
-      this.setState({loading: false})
-    }
+    };
 
-    const error = (err) => {
+    const error = err => {
       // console.log('ERROR')
       // console.log(err.message)
-      this.setState({loading: false})
-    }
+      this.setState({ loading: false });
+    };
 
-    navigator.geolocation.getCurrentPosition(success, error, options)
-  }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  };
 
-  render(){
-    return(
-      // <button onClick={this.getLocation} type='button'>get pos</button>
-
-      <button onClick={this.getLocation} className='CurrLocBtn' type='button'></button>
-    )
+  render() {
+    return (
+      <button
+        onClick={this.getLocation}
+        className="CurrLocBtn"
+        type="button"
+      ></button>
+    );
   }
 }
