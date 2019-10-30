590ea5956e59c7f5bc99581a38b3f3f1d07b3cbe
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-30 12:01:29 -0400

    remove unused imports from LandingPage

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Search from '../../components/Search/Search'
-import SearchResults from '../../components/SearchResults/SearchResults'
-import WeatherBar from '../../components/WeatherBar/WeatherBar'
 
 export default class LandingPage extends React.Component{
 
