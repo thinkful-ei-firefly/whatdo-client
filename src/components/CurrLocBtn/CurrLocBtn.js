d1de0937f2350254003a6278e3e4a1be8df8e242
krill <lpmaze@gmail.com>
2019-10-30 13:09:22 -0400

    modified CurrLocBtn css & EventsPage.css

--- src/components/CurrLocBtn/CurrLocBtn.js
+++ src/components/CurrLocBtn/CurrLocBtn.js
@@ -2,6 +2,8 @@ import React from 'react'
 import GeocodingApiService from '../../services/geocoding-api-service'
 // import {SearchProvider} from '../../contexts/SearchContext'
 
+import './CurrLocBtn.css'
+
 export default class CurrLocBtn extends React.Component{
   // static contextType = SearchContext
   state = {loading: false}
@@ -34,12 +36,13 @@ export default class CurrLocBtn extends React.Component{
 
     // eslint-disable-next-line no-unused-vars
     const currLoc = navigator.geolocation.getCurrentPosition(success, error, options)
-    
   }
 
   render(){
     return(
-      <button onClick={this.getLocation}>Current Location</button>
+      // <button onClick={this.getLocation} type='button'>get pos</button>
+
+      <button onClick={this.getLocation} className='CurrLocBtn' type='button'></button>
     )
   }
 }
