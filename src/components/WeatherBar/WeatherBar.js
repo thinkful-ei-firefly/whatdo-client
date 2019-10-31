613afbbe6785860e0e23210bfe09f6512ab55faa
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-31 10:41:20 -0400

    filter weather data to only include hours from 8am-11pm

--- src/components/WeatherBar/WeatherBar.js
+++ src/components/WeatherBar/WeatherBar.js
@@ -14,8 +14,14 @@ export default class WeatherBar extends React.Component {
     const dailyWeather = this.context.weather.filter(snapshot => {
       const dateTime = new Date(snapshot.dt);
       const searchDate = new Date(`${this.context.date}T12:00:00`);
-      return dateTime.toLocaleDateString() === searchDate.toLocaleDateString();
+      return (
+        dateTime.toLocaleDateString() === searchDate.toLocaleDateString() 
+        && dateTime.getHours() <= 23 
+        && dateTime.getHours() >= 8
+      )
     });
+
+    console.log(dailyWeather)
 
     if (dailyWeather.length > 0) {
       const weatherSnapshots = dailyWeather.map(snapshot => (
