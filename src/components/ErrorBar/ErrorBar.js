4d35fc411aa71abf9c3cf2fd964308cf11db70ee
Jess Doyle <jessica.e.doyle@gmail.com>
2019-11-01 12:10:18 -0400

    style error display bar and remove redundant imports

--- src/components/ErrorBar/ErrorBar.js
+++ src/components/ErrorBar/ErrorBar.js
@@ -6,7 +6,7 @@ export default class ErrorBar extends React.Component {
   render () {
     return (
       <div className="fetch-error-bar">
-        <h2>We're sorry, something went wrong.</h2>
+        <h2 className="error-bar-header">We're sorry, the server is experiencing some difficulties.</h2>
         <h3>Please wait a few minutes or refresh the page and try again.</h3>
       </div>
     );
--- src/routes/EventsPage/EventsPage.js
+++ src/routes/EventsPage/EventsPage.js
@@ -6,7 +6,6 @@ import './EventsPage.css'
 import SearchContext from '../../contexts/SearchContext'
 import ErrorBar from '../../components/ErrorBar/ErrorBar'
 import Loading from '../../components/Loading/Loading'
-import PageBar from '../../components/PageBar/PageBar'
 
 export default class EventsPage extends React.Component {
   static contextType = SearchContext
