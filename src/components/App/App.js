4da06164d1a0a8ee688e18bfc685641eac64b969
Jess Doyle <jessica.e.doyle@gmail.com>
2019-11-01 10:38:46 -0400

    fix to getDate() method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -58,12 +58,12 @@ class App extends React.Component {
 
   getDate() {
     const today = new Date();
-    const date =
-      today.getFullYear() +
-      '-' +
-      (today.getMonth() + 1) +
-      '-' +
-      today.getDate();
+    let year = today.getFullYear();
+    let month = today.getMonth()+1;
+    if (month < 10) month = `0${month}`
+    let day = today.getDate();
+    if (day < 10) day = `0${day}`
+    const date =`${year}-${month}-${day}`
     return date;
   }
 
